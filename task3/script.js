f705cbc0b75bdc78aee159ee3f4bb2e3608a4805
Margarita Puzanova <margarett.puzanova@gmail.com>
2022-09-23 18:15:02 +0300

    =Task 1 add Bootstrap

---
+++ task3/script.js
@@ -0,0 +1,31 @@
+const shoppingList = ['bread', 'orange', 'sausage', 'cake', 'butter'];
+
+const bread = {
+  name: 'rye bread',
+  quantity: 1,
+  isBought: 'yes'
+};
+
+const orange = {
+  name: 'moroccan orange',
+  quantity: 3,
+  isBought: 'no'
+};
+
+const sausage = {
+  name: 'meat sausage',
+  quantity: 8,
+  isBought: 'yes'
+};
+
+const cake = {
+  name: 'carrot cake',
+  quantity: 4,
+  isBought: 'yes'
+};
+
+const butter = {
+  name: 'butter',
+  quantity: 2,
+  isBought: 'no'
+};
