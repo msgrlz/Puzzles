1d447e54cc92db0f658f857b158b026fe3a8e82d
Margarita Puzanova <margarett.puzanova@gmail.com>
2022-10-01 16:50:34 +0300

    Study array

--- task3/script.js
+++ task3/script.js
@@ -1,57 +1,71 @@
-let bread = {
-  name: 'rye bread',
-  quantity: 1,
-  isBought: true
+// const products = [
+//   { name: 'bread', quantity: 1, isBought: 'yes' },
+//   { name: 'orange', quantity: 3, isBought: 'no' },
+//   { name: 'sausage', quantity: 8, isBought: 'yes' },
+//   { name: 'cake', quantity: 4, isBought: 'yes' },
+//   { name: 'butter', quantity: 2, isBought: 'no' }
+// ];
+
+
+// // 1 way
+// const productsAvailable = [];
+
+// for (const product of products) {
+//   if (product.isBought == 'yes') {
+//     productsAvailable.push(product)
+//   }
+// }
+// console.log(productsAvailable);
+
+// // 2 way
+
+// const productsAvailable = products.filter(item => {
+//   if (item.isBought === 'yes') {
+//     return true;
+//   }
+// });
+// console.log(productsAvailable);
+
+// //1. сложить объекты в массив
+// //foreach
+// // 1. Вывод списка продуктов: сначала купленные, потом нет.
+
+
+// // const outputList = shoppingList.filter(i => i.isBought == true);
+
+// // 2. Добавление покупки в список. При добавлении покупки с уже существующим в списке продуктом,
+// //  необходимо увеличивать количество в существующей покупке, а не добавлять новую.
+
+
+
+// // 3. Покупка продукта. Функция принимает название продукта и отмечает его как купленный.
+
+// const buyProduct = () => {
+
+// }
+
+
+// Сортировка слов по глпсным буквам.
+
+const words = ['Коробка', 'Яблоко', 'Йод', 'апельсин', 'Работа', 'Театр', 'Ёжик', 'Урна','Облако', 'Собака', 'Енот'];
+
+const sortVowels = (arrWords = []) => {
+  const vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'];
+  return arrWords.sort().reverse().sort((wordA, wordB) => {
+    const firstLetterA = wordA[0].toLowerCase();
+    const firstLetterB = wordB[0].toLowerCase();
+
+      if (vowels.includes(firstLetterA)) {
+        return -1;
+      }
+
+      if (vowels.includes(firstLetterB)) {
+        return 1;
+      }
+
+        return 0;
+  });
+
+
 };
-
-let orange = {
-  name: 'moroccan orange',
-  quantity: 3,
-  isBought: false
-};
-
-let sausage = {
-  name: 'meat sausage',
-  quantity: 8,
-  isBought: true
-};
-
-let cake = {
-  name: 'carrot cake',
-  quantity: 4,
-  isBought: true
-};
-
-let butter = {
-  name: 'butter',
-  quantity: 2,
-  isBought: false
-};
-
-const shoppingList = [ bread, orange, sausage, cake, butter ];
-
-
-// 1. Вывод списка продуктов: сначала купленные, потом нет.
-
-const outputList = () => {
-
-  if (shoppingList[i].isBought == true) {
-    shoppingList.unshift(i);
-  } else {
-    shoppingList.push(i);
-  }
-  console.log(outputList.length);
-};
-outputList();
-// const outputList = shoppingList.filter(i => i.isBought == true);
-
-// 2. Добавление покупки в список. При добавлении покупки с уже существующим в списке продуктом,
-//  необходимо увеличивать количество в существующей покупке, а не добавлять новую.
-
-
-
-// 3. Покупка продукта. Функция принимает название продукта и отмечает его как купленный.
-
-const buyProduct = () => {
-
-}
+console.log(sortVowels(words));
