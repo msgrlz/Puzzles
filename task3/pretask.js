9543d4512127915f5920ca6f3878053df815cf56
taraswww777 <taraswww777@mail.ru>
2022-10-10 18:37:42 +0300

    fix after conflict

--- task3/pretask.js
+++ task3/pretask.js
@@ -48,55 +48,42 @@
 
 
 
-const sortVowels = (arrWords = []) => { // Создаем стрелочную функцию sortVowels, принимающую как аргумент пустой массив arrWords.
-  const vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']; // Внутри функции создаем массив vowels, состоящий из гласных букв русского алфавита.
-  const preparedWords = arrWords.map((word) => { // Также создаем еще одну функцию preparedWords, в которой применим к пустому массиву arrWords метод map() для формирования нового массива путем вызова переданной функции один раз для каждого элемента. Аргумент word = item.
-    return word.toLowerCase(); // Возвращает элемент word в нижнем регистре.
-  });
-  return preparedWords.sort((wordA, wordB) => { // Возвращаем функцию preparedWords, сразу применяя к ней метод sort(), сравнивая первый строчный элемент со вторым в порядке следования кодовых символов Unicode.
-
-    const firstLetterA = wordA[0]; // Создаем две переменные, присваивающие по первому символу(букве) элементы wordA и wordB.
-    const firstLetterB = wordB[0];
-
-    // const upperVowels = vowels.toUpperCase();// создаю новую переменную
-
-    if (vowels.includes(firstLetterA)) { // Применяем к массиву гласных букв vowels метод includes(), определяющий - содержит ли массив искомый элемент. Сравниваем: если первый символ(буква) первого слова содержит любую букву из массива vowels, то складываем их в конец  массива.
-      return 1;
-    }
-
-    if (vowels.includes(firstLetterB)) {
-      return -1;
-    }
-
-    return -1;
-  });
-
-
-  const sortVowels = (arrWords = []) => {
-    const vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'];
-    const preparedWords = arrWords.map(word => ({
+const sortVowels = (arrWords = []) => {
+  // Создаем стрелочную функцию sortVowels, принимающую как аргумент пустой массив arrWords.
+  const vowels = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я'];
+  // Внутри функции создаем массив vowels, состоящий из гласных букв русского алфавита.
+  // Также создаем еще одну функцию preparedWords, в которой применим к пустому массиву arrWords метод map()
+  // для формирования нового массива путем вызова переданной функции один раз для каждого элемента.
+  // Аргумент word = item.
+    // Возвращает элемент word в нижнем регистре.
+  const preparedWords = arrWords.map((word) => {
+    const resultItem = {
       word,
       lowerWord: word.toLowerCase()
-    }));
-    const otherWords = [];
+    };
 
-    const vowelsWords = preparedWords.map((word) => {
-      const firstLetter = word.lowerWord[0];
-      if (vowels.includes(firstLetter)) {
-        return word;
-      }
-      otherWords.push(word.word);
-      return undefined;
-    }).filter(item => Boolean(item)).sort((wordA, wordB) => (
+    return resultItem;
+  });
+
+  const otherWords = [];
+
+  const vowelsWords = preparedWords.map((word) => {
+    const firstLetter = word.lowerWord[0];
+    if (vowels.includes(firstLetter)) {
+      return word;
+    }
+    otherWords.push(word.word);
+    return undefined;
+  }).filter(item => Boolean(item)).sort((wordA, wordB) => (
       wordA.lowerWord[0] > wordB.lowerWord[0] ? 1 : -1
-    )).map(({ word }) => word);
+  )).map(({ word }) => word);
 
-    return [
-      ...vowelsWords,
-      ...otherWords
-    ];
-  };
+  return [
+    ...vowelsWords,
+    ...otherWords
+  ];
+}
 
-  const words = ['Коробка', 'Яблоко', 'яйца', 'Йод', 'апельсин', 'Работа', 'Театр', 'Ёжик', 'Урна', 'Облако', 'Собака', 'Енот'];
+const words = ['Коробка', 'Яблоко', 'яйца', 'Йод', 'апельсин', 'Работа', 'Театр', 'Ёжик', 'Урна', 'Облако', 'Собака', 'Енот'];
 
-  console.log(sortVowels(words)); // Выводим в консоль отсортированный массив words и любуемся результатом!
+console.log(sortVowels(words)); // Выводим в консоль отсортированный массив words и любуемся результатом!
